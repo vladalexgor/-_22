e20e2a963c1bc59f40f1e49a8d3aaf4f79ace179
vladalexgor@ya.ru <vladalexgor@ya.ru>
2021-12-29 19:04:08 +0300

    Добавьте файлы проекта.

---
+++ Задание_22/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_22
+{
+    //Сформировать массив случайных целых чисел (размер задается пользователем).
+    //Вычислить сумму чисел массива и максимальное число в массиве.
+    //Реализовать решение задачи с использованием механизма задач продолжения.
+    class Program
+    {
+        public class Array
+        {
+            const int a = -100;
+            const int b = 100;
+            int n;
+            public int N
+            {
+                get
+                {
+                    return n;
+                }
+                set
+                {
+                    if (value < 1)
+                    {
+                        n = 1;
+                    }
+                    else
+                    {
+                        n = value;
+                    }
+                }
+            }
+            int[] array;
+            public Array(int n1)
+            {
+                N = n1;
+                Random random = new Random();
+                array = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    array[i] = random.Next(a, b);
+                    Console.Write("{0} ", array[i]);
+                }
+            }
+            public void Sum()
+            {
+                int sum = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    sum += array[i];
+                }
+                Console.WriteLine($"Сумма чисел массива: Sum = {sum}");
+            }
+            public void Max(Task task)
+            {
+                int max = a;
+                for (int i = 0; i < n; i++)
+                {
+                    if (array[i] > max)
+                    {
+                        max = array[i];
+                    }
+                }
+                Console.WriteLine($"Максимальное число в массиве: Max = {max}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите положительное целое число, равное количеству элементов массива целых чисел:");
+                int n = Convert.ToInt32(Console.ReadLine());
+                Array array = new Array(n);
+                Console.WriteLine();
+                Action action = new Action(array.Sum);
+                Task task1 = new Task(action);
+                Action<Task> actionTask = new Action<Task>(array.Max);
+                Task task2 = task1.ContinueWith(actionTask);
+                task1.Start();
+                task2.Wait();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("Для завершения программы нажмите любую клавишу");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задание_22_Примеры/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Задание_22_Примеры
+{
+    class Program
+    {
+        static void Method1()
+        {
+            Console.WriteLine("Method1 начал работу");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Method1 выводит счетчик = {i}");
+                Thread.Sleep(100);
+            }
+            Console.WriteLine("Method1 окончил работу");
+        }
+        static void Method2(Task task, object a)
+        {
+            int n = (int)a;
+            Console.WriteLine("Method2 начал работу");
+            for (int i = n; i < n+10; i++)
+            {
+                Console.WriteLine($"Method2 выводит счетчик = {i}");
+                Thread.Sleep(800);
+            }
+            Console.WriteLine("Method2 окончил работу");
+        }
+        static int Method3()
+        {
+            Console.WriteLine("Method3 начал работу");
+            int S = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                S += i;
+                Thread.Sleep(500);
+            }
+            Console.WriteLine("Method3 окончил работу");
+            return (S);
+        }
+        static int Method4(int a)
+        {
+            Console.WriteLine("Method4 начал работу");
+            int S = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                S += a;
+                Thread.Sleep(500);
+            }
+            Console.WriteLine("Method4 окончил работу");
+            return (S);
+        }
+        static void Main(string[] args)
+        {
+
+            //Task task = new Task(() => Console.WriteLine("Hello!")); - лямбда выражение
+
+            //Task task = new Task(delegate () { Console.WriteLine("Hello!"); }); - анонимный метод
+
+            //Task task = new Task(Method1); - техника предположения делегата
+
+            //Action action = new Action(Method1);
+            //Task task = new Task(action);
+
+            //task.Start();
+
+            //Task task = Task.Factory.StartNew(action);
+
+            //Task task = Task.Run(action);
+
+            //Task task = new Task(() => Method2(100));
+            //task.Start();
+
+            //Func<int> func = new Func<int>(Method3);
+            //Task<int> task = new Task<int>(func);
+            //task.Start();
+
+            //Method1();
+            //Console.WriteLine(task.Result);
+            //task.Wait();
+
+            Action action = new Action(Method1);
+            Task task1 = new Task(action);
+
+            Action<Task, object> actionTask = new Action<Task, object>(Method2);
+            Task task2 = task1.ContinueWith(actionTask, 100);
+
+            task1.Start();
+
+            Console.WriteLine("Main окончил работу");
+            Console.ReadKey();
+        }
+    }
+}
